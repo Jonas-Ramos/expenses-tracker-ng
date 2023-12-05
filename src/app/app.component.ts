d2fb3749f06f0aa888f6cad45f03eb54182aac18
Jony Monroy <jonymonroy@gmail.com>
2023-12-05 09:39:12 -0500

    Angular: One way & Two way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
 import { BalanceComponent } from './components/balance/balance.component';
 
 @Component({
@@ -12,5 +11,5 @@ import { BalanceComponent } from './components/balance/balance.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Frontend Con Angular';
+
 }
--- src/app/components/balance/balance.component.ts
+++ src/app/components/balance/balance.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-balance',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './balance.component.html',
-  styleUrl: './balance.component.scss'
+  styleUrl: './balance.component.scss',
 })
 export class BalanceComponent {
+  // Variable implícita
+  title: string = "The Balance Component";
 
+  // Variable explícita
+  search: string = "Buscar...";
 }
