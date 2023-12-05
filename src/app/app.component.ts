a465da2c0e2f7057face72f59eb24c7e4f0d4eaf
Jony Monroy <jonymonroy@gmail.com>
2023-12-05 09:06:43 -0500

    Angular: Creación y uso del componente Balance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { BalanceComponent } from './components/balance/balance.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, BalanceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular17-expenses-tracker';
+  title = 'Frontend Con Angular';
 }
