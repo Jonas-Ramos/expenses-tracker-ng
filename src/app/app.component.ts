255fc6e57b37ea0a9c20845ff5c3d2d3bc2db486
Jony Monroy <jonymonroy@gmail.com>
2023-12-13 18:10:48 -0500

    Angular: Input basado en una interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BalanceComponent } from './components/balance/balance.component';
+import { Balance } from './models/balance.model';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,9 @@ import { BalanceComponent } from './components/balance/balance.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
+    balance: Balance  = {
+    amount : 55_000,
+    income : 100_000,
+    expenses : 45_000,
+  }
 }
