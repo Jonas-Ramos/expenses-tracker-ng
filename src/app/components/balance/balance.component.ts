722a068fabc759a08cf5dfb83efb562c90d8e453
Jony Monroy <jonymonroy@gmail.com>
2023-12-13 17:36:56 -0500

    Angular: Definición y uso de atributo del componente basado en una interface

--- src/app/components/balance/balance.component.ts
+++ src/app/components/balance/balance.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Balance } from '../../models/balance.model';
 
 
 @Component({
@@ -11,5 +12,10 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './balance.component.scss',
 })
 export class BalanceComponent {
-  @Input() title: string = "Balance Component"
+  @Input() title: string = 'Título';
+  @Input() balance: Balance = {
+    amount : 55_000,
+    income : 100_000,
+    expenses : 45_000,
+   };
 }
